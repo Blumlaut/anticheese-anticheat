e05b9f81ca79b1a991b1f3802fc394081b7057a0
lizarragaeneko <67381650+lizarragaeneko@users.noreply.github.com>
2021-07-25 15:22:43 +0200

    Create config.lua
    Add New Configs for server side events that i will add.

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {
+    ChatLogs = "", --Discord Webhook
+    ConnectionLogs = "", --Discord Webhook
+    DeadLogs = "", --Discord Webhook
+    AntiVPN = true, --ACTIVATES A VPN/PROXY Blocker
+}
