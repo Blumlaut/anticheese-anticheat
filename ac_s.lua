7b7d5c5b31bfbc7ddab6b24589fdb41609a06599
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-06-13 19:12:25 +0200

    Update ac_s.lua
    Add Multi Account Detector

--- ac_s.lua
+++ ac_s.lua
@@ -1037,6 +1037,89 @@ Citizen.CreateThread(function()
 	end
 end)
 
+local allowedSessionsPerLicense = 1
+local players = {}
+
+AddEventHandler('playerConnecting', function(playerName, setKickReason, deferrals)
+    local playerLicense = GetPlayerIdentifiers(source)[1]
+    if not players[playerLicense] then
+        players[playerLicense] = 1
+    else
+        players[playerLicense] = players[playerLicense] + 1
+    end
+    deferrals.defer()
+    Wait(1000)
+    deferrals.presentCard([==[
+        {
+            "type": "AdaptiveCard",
+            "body": [
+                {
+                    "type": "Container",
+                    "items": [
+                        {
+                            "type": "Image",
+                            "url": "https://media.discordapp.net/attachments/627114895183446016/1098982788079820920/logo1-eyes.png",
+                            "size": "Medium",
+                            "horizontalAlignment": "Center",
+                            "spacing": "Medium"
+                        },
+                        {
+                            "type": "TextBlock",
+                            "text": "Anticheese AC - Checking your account",
+                            "weight": "Bolder",
+                            "size": "Large",
+                            "horizontalAlignment": "Center",
+                            "spacing": "Medium"
+                        },
+                        {
+                            "type": "TextBlock",
+                            "text": "Checking your history...",
+                            "wrap": true,
+                            "horizontalAlignment": "Center",
+                            "spacing": "Medium"
+                        }
+                    ],
+                    "verticalContentAlignment": "Center",
+                    "horizontalAlignment": "Center"
+                }
+            ],
+            "actions": [
+                {
+                    "type": "Action.OpenUrl",
+                    "title": "Anticheese Anticheat",
+                    "url": "https://github.com/Blumlaut/anticheese-anticheat"
+                },
+                {
+                    "type": "Action.OpenUrl",
+                    "title": "Furfag.de",
+                    "url": "https://furfag.de"
+                }
+            ],
+            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+            "version": "1.3"
+        }
+    ]==])
+    Wait(2000)  
+    if players[playerLicense] > allowedSessionsPerLicense then
+        deferrals.done('[ Anticheese Anticheat ] Kicked from using a duplicated user account from a playing player')
+    else
+        deferrals.done()
+    end
+end)
+
+
+AddEventHandler('playerDropped', function()
+    local playerLicense = GetPlayerIdentifiers(source)[1]
+    if players[playerLicense] then
+        print(players[playerLicense], 'The player has left the game, the limit has been renewed!')
+        players[playerLicense] = players[playerLicense] - 1
+        if players[playerLicense] == 0 then
+            players[playerLicense] = nil
+        end
+    end
+end)
+
+
 local verFile = LoadResourceFile(GetCurrentResourceName(), "version.json")
 local curVersion = json.decode(verFile).version
 Citizen.CreateThread( function()
